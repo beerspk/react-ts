ad3a6660ad770dae823b66f0de0c7e9eb79fbefe
Pamorn.s <pamorn.si@gmail.com>
2023-03-10 10:27:09 +0700

    'init'

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Todos from './components/Todos'
+import NewTodo from './components/NewTodo'
+import TodosContextProvider from './store/todo-context'
+
+function App() {
+  return (
+    <TodosContextProvider>
+      <NewTodo />
+
+      <Todos />
+    </TodosContextProvider>
+  )
+}
+
+export default App
